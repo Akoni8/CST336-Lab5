01aca9ffc056fd4f2934d3dac5f36456af087eb2
Akoni <hakmorrison@csumb.edu>
2017-10-17 22:46:56 +0000

    updated styles

--- index.php
+++ index.php
@@ -117,10 +117,10 @@
         <style type="text/css">
            @import url("styles.css"); 
         </style>
-          
+         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
-        <div id="container">
+        <div id="contain">
              <h1> Technology Checkout</h1>
              <form>
                 <span>Device:</span>
